1727dfcff10dfda2fc45fb57e2e7ab3920160fb6
Farhan Tahmid <farhantahmid007@gmail.com>
2020-05-22 04:24:06 -0500

    Publish changes 😁

---
+++ README.md
@@ -0,0 +1,3 @@
+#Background-Generator
+
+A simple web app to make a gradient background. Change the two color tabs to get a new gradient background. The CSS code is shown below for using on other platforms.
--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+// To put the CSS code in the blank h3 tag
 var css = document.querySelector("h3");
+
+// Select the first color class
 var color1 = document.querySelector(".color1");
+
+// Select the second color class
 var color2 = document.querySelector(".color2");
+
+// Fetch the body to change the gradient
 var body = document.getElementById("gradient");
 
+// Write the function to change the gradient
 function setGradient() {
     body.style.background = "linear-gradient(to right, " 
     + color1.value 
     + ", " 
     + color2.value 
     + ")";
-
+    // Assign the h3 to the CSS color code
     css.textContent = body.style.background + ";";
 }
 
+// Callback functions
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
